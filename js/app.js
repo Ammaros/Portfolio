32d92c7083c33f90d75a1b77ce92de5c54a9e819
Ammaros <ammarstudent2000@gmail.com>
2020-12-18 11:52:56 +0500

    Links and Colours Updated

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// const colorThief = new ColorThief();
+// const img = document.querySelector('img');
+
+// // Make sure image is finished loading
+// if (img.complete) {
+//     colorThief.getColor(img);
+// } else {
+//     image.addEventListener('load', function() {
+//     colorThief.getColor(img);
+//     });
+// }
