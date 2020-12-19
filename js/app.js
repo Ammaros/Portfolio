c278b65f1cb62ab8eed987259569bf3aaff26838
Ammaros <ammarstudent2000@gmail.com>
2020-12-19 08:53:17 +0500

    Cursor Effect

--- js/app.js
+++ js/app.js
@@ -9,3 +9,20 @@
 //     colorThief.getColor(img);
 //     });
 // }
+
+// Cursor
+let cursor = document.getElementById("cursor");
+let primaryBtn = document.getElementsByClassName("btn-primary")[0];
+
+document.addEventListener("mousemove", (pos) => {
+    cursor.style.left = pos.clientX + "px"
+    cursor.style.top = (pos.clientY + scrollY) + "px"
+});
+
+primaryBtn.addEventListener("mouseover", () => {
+    cursor.classList.add("cursor-hover");
+});
+
+primaryBtn.addEventListener("mouseleave", () => {
+    cursor.classList.remove("cursor-hover");
+});
